9667827fc4de8cb827163375f18f5081846185e1
MontenegroCruzUK <montenegrocruz.uk@gmail.com>
2023-12-15 01:33:39 +0000

    Implementacion de ventanas

--- src/main/java/com/example/hotelhavana/controller/Dashboard_Controller.java
+++ src/main/java/com/example/hotelhavana/controller/Dashboard_Controller.java
@@ -1,14 +1,20 @@
 package com.example.hotelhavana.controller;
 
 import com.example.hotelhavana.ResourcesLoader;
+import io.github.palexdev.materialfx.controls.MFXIconWrapper;
+import io.github.palexdev.materialfx.controls.MFXRectangleToggleNode;
 import io.github.palexdev.materialfx.utils.ScrollUtils;
 import io.github.palexdev.materialfx.utils.ToggleButtonsUtil;
+import io.github.palexdev.materialfx.utils.others.loader.MFXLoader;
+import io.github.palexdev.materialfx.utils.others.loader.MFXLoaderBean;
 import javafx.application.Platform;
 import javafx.css.PseudoClass;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.geometry.Pos;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.ToggleButton;
 import javafx.scene.control.ToggleGroup;
 import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
@@ -24,6 +30,7 @@ import javafx.scene.control.ToggleGroup;
 
 import java.io.IOException;
 import java.net.URL;
+import java.util.List;
 import java.util.ResourceBundle;
 
 public class Dashboard_Controller implements Initializable {
@@ -89,6 +96,47 @@ public class Dashboard_Controller implements Initializable {
 	}
 	
 	private void initializeLoader () {
+		MFXLoader loader = new MFXLoader ();
+		
+		loader.addView (MFXLoaderBean.of ("Nueva Venta",
+			ResourcesLoader.loadURL ("view/Jornada_View.fxml")).setBeanToNodeMapper (() -> {
+			return this.createToggle ("mfx-square-pen", "Jornada");
+		}).setDefaultRoot (true).get ());
+		
+		loader.addView (
+			MFXLoaderBean.of ("Banquete", ResourcesLoader.loadURL ("view/Banquete_View.fxml")).setBeanToNodeMapper (
+				() -> createToggle ("mfx-users", "Banquete")).get ());
+		
+		loader.addView (
+			MFXLoaderBean.of ("Congreso", ResourcesLoader.loadURL ("view/Congreso_View.fxml")).setBeanToNodeMapper (
+				() -> createToggle ("mfx-users", "Congreso")).get ());
+		
+		
+		loader.setOnLoadedAction ((beans) -> {
+			List<ToggleButton> nodes = beans.stream ().map ((bean) -> {
+				ToggleButton toggle = (ToggleButton) bean.getBeanToNodeMapper ().get ();
+				toggle.setOnAction ((event) -> {
+					this.contentPane.getChildren ().setAll (bean.getRoot ());
+				});
+				if (bean.isDefaultView ()) {
+					this.contentPane.getChildren ().setAll (bean.getRoot ());
+					toggle.setSelected (true);
+				}
+				
+				return toggle;
+			}).toList ();
+			this.navBar.getChildren ().setAll (nodes);
+		});
+		loader.start ();
+	}
+	
+	private ToggleButton createToggle (String icon, String text) {
+		MFXIconWrapper wrapper = new MFXIconWrapper (icon, 24.0, 32.0);
+		MFXRectangleToggleNode toggleNode = new MFXRectangleToggleNode (text, wrapper);
+		toggleNode.setAlignment (Pos.CENTER_LEFT);
+		toggleNode.setToggleGroup (this.toggleGroup);
+		
+		return toggleNode;
 	}
 	
 	public void closeWindows () {
